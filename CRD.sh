3ad4d0df6c8b151d2efe89ac0191966445a76c6d
kabilGamer <40713146+kabilGamer@users.noreply.github.com>
2021-08-16 21:37:10 +0100

    Update CRD.sh

--- CRD.sh
+++ CRD.sh
@@ -18,7 +18,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser b4iterdev chrome-remote-desktop
+sudo adduser kabildja chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete \nCreated new user kabildja with password:kabildja \nYou can change both kabildja's password with passwd(with no sudo) \nOr you can change root's password with sudo passwd" >&2 ||
 printf "\nError Occured " >&2
